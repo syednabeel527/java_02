24625b46ca2a23996cfa5fd730e9a7000f89a76d
syednabeel527 <syednabeelsu2005@gmail.com>
2025-11-28 18:54:47 +0530

    programs in java from basic to advance

---
+++ program2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class program2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("The number entered is: "+a);
+        int b=sc.nextInt();
+        System.out.println("The number entered is: "+b);
+        System.out.println("sum is: "+(a+b));
+    }
+}
