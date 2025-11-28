32e90387dbb9a44aa38c5c644f01ece576e262f4
syednabeel527 <syednabeelsu2005@gmail.com>
2025-11-28 22:44:31 +0530

    programs in java from basic to advance

---
+++ program3.java
@@ -0,0 +1,17 @@
+public class program3 {
+    public static void main(String args[]) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        System.out.println(var);
+        
+        int number = 25;
+        System.out.println(number);
+        //long
+        //double
+        short n = 240;
+        System.out.println(n);
+    }
+}
---
+++ program5.java
@@ -0,0 +1,12 @@
+public class program5 {
+    public static void main(String args[]) {
+        int a = 10;
+        // type conversion
+        float b = a;
+        System.out.println(b);
+        // type casting
+        float aa = 10.27f;
+        int ab = (int) aa;
+        System.out.println(ab);
+    }
+}
